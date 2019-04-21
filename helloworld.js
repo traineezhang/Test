be4c71ab51f871073baf4fa91ab0ca38cf0779fb
zhangyuntao <821866745@qq.com>
2019-04-21 17:20:40 +0800

    first commit

---
+++ helloworld.js
@@ -0,0 +1,93 @@
+//alert("helloworld");
+//弹出的窗口
+
+//单行注释
+
+//在控制台打印
+console.log("Hello world");
+console.log(123);
+console.log(true);
+var greeting="hello";
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1,b:2});
+
+console.table({a:1,b:2});
+
+console.error("this is some error");
+
+console.clear();
+// 清除控制台
+
+console.warn("This is a warning");
+
+
+/*多行注释
+*/
+
+
+
+/*  三种容器关键字
+ var， let， const
+
+定义变量
+var name="变量所存储的值！"；
+console.log(name);  打印值 name
+
+修改name变量中的内容
+name=‘米斯特吴’；
+console。log(name);
+
+初始化var
+var greeting;
+console.log(greeting);  控制台上显示为 undefined  因为没有赋值
+greeting = 'hello';
+console.log(greeting);
+
+变量名的要求：字母，数字，_,$
+不能以数字开头
+var _name=123
+
+书写变量的多种形式
+var firstName = 'wu';
+var first_Name = 'wu';
+var FirstName = 'wu';
+var firstname = 'wu';
+
+
+快捷注释 ctrl+/
+
+let
+let name;
+name = 'miss';
+console.log(name);
+name = '海洋';
+console.log(name);
+
+
+CONST 常量关键字
+const name = 'miss';
+console.log(name);
+不能修改常量的值
+name = '海洋';
+定义常量一定要赋值
+const greeting;
+
+
+const person = {
+  name:'miss',
+  age: 32
+};
+
+
+下面这个可以改变
+person.name = '海洋';
+person。age = 31;
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers = [1,2,3,4,5,6];  错误的，无法生效
+numbers.push(6); 数组中添加6
+console.log(numbers);
+
+*/
